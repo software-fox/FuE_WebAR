e8af4f56ffd10dc22f212bb83bdc84a51ad7809f
ToBa1983 <torsten.baschwitz@gmx.net>
2020-11-09 13:57:57 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: ['vue-style-loader', /*'css-loader'*/ { loader: 'css-loader', options: { esModule: false }}]},
+      { test: /\.(jpg|png)$/, use: { loader: 'url-loader'}},
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};
