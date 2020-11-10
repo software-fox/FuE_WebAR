5e50034dea3d39733c42a4a070e71f055b3e17c7
ToBa1983 <torsten.baschwitz@gmx.net>
2020-11-10 18:46:28 +0100

    three js integration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     {{ message }}
+    <vue-ar />
   </div>
 </template>
 
 <script>
+import VueAr from "./vue-components/vue-ar.vue";
 export default {
+  name: "App",
   data() {
     return {
       message: "Hello World",
     };
+  },
+  components: {
+    VueAr,
+  },
+  mounted() {
+    console.log("mounted");
+    
   },
 };
 </script>
---
+++ src/vue-components/vue-ar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    <canvas id="yoghurt"></canvas>
+    <script
+      type="module"
+      src="..\src\scripts\web-ar-example.js"
+    ></script>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "VueAr",
+  components: {
+  },
+  mounted() {
+    console.log("mounted vue ar component");
+    
+  },
+};
+</script>
+
+<style >
+#yoghurt{
+  width: 70vw;
+  height: 70vh;
+}
+</style>
